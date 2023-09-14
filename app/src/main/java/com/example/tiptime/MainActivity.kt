25361db2e81a4cafeda16d00b1e22e14155f9790
Brayan Díaz <s21120009@alumnos.itsur.edu.mx>
2023-09-14 09:11:28 -0600

    Se corrigieron mas errores para que compilara el codigo y hacerlo funcional.

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -15,7 +15,6 @@
  */
 package com.example.tiptime
 
-import android.icu.text.AlphabeticIndex.Bucket.LabelType
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -34,7 +33,6 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -48,11 +46,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
 import java.text.NumberFormat
@@ -79,11 +77,10 @@ fun TipTimeLayout() {
     var amountInput by remember { mutableStateOf("") }
     var tipInput by remember { mutableStateOf("") }
     var roundUp by remember { mutableStateOf(false) }
-
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
     val tip = calculateTip(amount, tipPercent, roundUp)
-
+    val focusManager = LocalFocusManager.current
     Column(
         modifier = Modifier
             .padding(40.dp)
@@ -163,21 +160,21 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boo
 }
 @Composable
 fun RoundTheTipRow(
-    roundUp:Boolean,
-    on : (Boolean)->Unit,
-    modifier: Modifier = Modifier){
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+){
     Row(
         modifier = modifier
             .fillMaxWidth()
             .size(48.dp),
-        verticalAlignment = Alignment.CenterVertically
     ){
         Text(text = stringResource(R.string.round_up_tip))
         Switch(
             modifier = modifier
                 .fillMaxWidth()
                 .wrapContentWidth(Alignment.End),
-            checked = roundUp, onCheckedChange = on)
+            checked = roundUp, onCheckedChange=onRoundUpChanged)
     }
 @Composable
 fun TipTimeLayoutPreview() {
